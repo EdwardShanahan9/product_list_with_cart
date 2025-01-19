0b4a4afa4d6e74c10c62e98f7f7d507b0801a415
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-01-19 15:52:16 +0000

    Add MainLayout
    - MainLayout holds the layout for Products and Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import MainLayout from "./layout/MainLayout/MainLayout";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl">Hello world!</h1>
+      <MainLayout />
     </>
   );
 }
---
+++ src/layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import Cart from "./Cart/Cart";
+import Products from "./Products/Products";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Products />
+      <Cart />
+    </div>
+  );
+};
+
+export default MainLayout;
