19a84d2462a662031482e3f44ebff869a4b8c058
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-01-19 15:35:08 +0000

    Add custom style to tailwind.config.js
    - Added customs colors
    - Added font RedHatText, regular, medium, and bold

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#c73a0f",
+        secondary: "##1ea475",
+        rose900: "#260f08",
+        rose500: "#87635a",
+        rose400: "#ad8985",
+        rose300: "#c9aea6",
+        rose100: "#f4edeb",
+        rose50: "#fcf9f7",
+      },
+      fontFamily: {
+        primaryRegular: ["Regular"],
+        primaryMedium: ["Medium"],
+        primaryBold: ["Bold"],
+      },
+    },
   },
   plugins: [],
 };
