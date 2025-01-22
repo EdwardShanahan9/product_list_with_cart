8118fd6840cab10b076b15f1c274833fa128f8f5
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-01-22 18:48:19 +0000

    Add acticeButton to Button component
    - When the add to cart button is clicked, the active button will display instead
    - Note the button style and functionality is not working yet

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import IconCart from "../../assets/images/icon-add-to-cart.svg";
+import IconAdd from "../../assets/images/icon-increment-quantity.svg";
+import IconMinus from "../../assets/images/icon-decrement-quantity.svg";
 
 const Button = () => {
+  const [isActive, setIsActice] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  const addButton = (
+    <>
+      <button
+        onClick={() => setIsActice(true)}
+        className="bg-white py-3 px-7 border border-rose400 rounded-full flex items-center text-rose900 font-primaryBold text-sm"
+      >
+        <img className="mr-2" src={IconCart} alt="Cart Icon" />
+        {"Add to Cart"}
+      </button>
+    </>
+  );
+  const activeButton = (
+    <>
+      <button className="bg-primary py-3 px-7 border border-primary rounded-full flex  items-center justify-between text-white font-primaryBold text-sm">
+        <img className="mr-11" src={IconMinus} alt="Cart Icon" />
+        <span className="mr-11">{counter}</span>
+        <img className="" src={IconAdd} alt="Cart Icon" />
+      </button>
+    </>
+  );
+
   return (
-    <button className="absolute bottom-2 translate-y-2/3  bg-white py-3 px-7 border border-rose400 rounded-full flex items-center text-rose900 font-primaryBold text-sm">
-      <img className="mr-2" src={IconCart} alt="Cart Icon" />
-      Add to Cart
-    </button>
+    <div className="absolute bottom-2 translate-y-2/3 ">
+      {isActive ? activeButton : addButton}
+    </div>
   );
 };
 
